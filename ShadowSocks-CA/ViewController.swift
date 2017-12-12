bf53672d180c0c4a2be9ea97845a09faf138b672
Lys <874907806@qq.com>
2017-12-12 14:13:16 +0800

    Initial Commit

---
+++ ShadowSocks-CA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowSocks-CA
+//
+//  Created by 李燕山 on 2017/12/12.
+//  Copyright © 2017年 cloudAtlas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
